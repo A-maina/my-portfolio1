b0e19890f3c16b9082cbbe979871e0e8f33525af
Mark Brian <markbkiunga2017@gmail.com>
2024-11-04 10:20:38 +0300

    Updated email credentials and fixed typo in Contact.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "markbkiunga2017@gmail.com",
+    pass: "kusmesazlskibxui"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "markbkiunga2017@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
