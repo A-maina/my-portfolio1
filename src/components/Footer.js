4da35c4c69339e6324bb160d8fe057d3afe9ef5f
Mark Brian <markbkiunga2017@gmail.com>
2024-11-04 10:32:33 +0300

    Updated styles for social icons and footer, changed logo image in Footer and NavBar components, and added links to social media profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/mark-logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -12,13 +12,13 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" class="logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/markbriankiungakamau"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.github.com/Markbkiunga"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/heismarkbkiunga"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
